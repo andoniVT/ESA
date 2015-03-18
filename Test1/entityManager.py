57089d8b7adb92360bf9f7ba674d35c69efede04
andoniVT <andoni.valverde@ucsp.edu.pe>
2015-03-18 11:47:01 -0500

    entities

--- Test1/entityManager.py
+++ Test1/entityManager.py
@@ -45,14 +45,76 @@ class EManager():
         else:
             print "Entidad ya existe"  
         self.save_file(self.__entities)
-
+    
+    def add_attribute(self, nombre_entidad, atributo):
+        for i in self.__entities:
+            if i[0] == nombre_entidad:
+                i[1].append(atributo)
+        self.save_file(self.__entities)
+    
+    def load_value(self, word):
+        lista = dict()
+        file = open(vocabulary, 'r')
+        file2 = open(vocabulary2, 'r')  
+        files = [file, file2]
+        for i in files:
+            while True:
+                line = i.readline()
+                if not line: break;
+                tmp = re.split("=", line)
+                lista[tmp[0]] = tmp[2].rstrip()
+        if lista.has_key(word):
+            return int(lista[word])
+        else:
+            return 0
+    
+    def update_polarity(self, nombre_entidad, value):
+        for i in self.__entities:
+            if i[0] == nombre_entidad:
+                for j in i[1]:
+                    if j[2] == "NULL":
+                        j[2] = value
+        self.save_file(self.__entities)
+    
+    def find_polarity(self, lista_entidades):
+        suma = 0.0
+        n = 0
+        for i in self.__entities:
+            for e in lista_entidades:
+                if i[0] == e:
+                    posicion = len(i[1])-1
+                    sentence = i[1][posicion][1]
+                    list_sentence = sentence.split()
+                    for j in list_sentence:
+                        suma+=self.load_value(j)
+                        n+=1
+                    value = suma/n
+                    print i[0]
+                    print value
+                    self.update_polarity(i[0], value)
+                    suma=0.0
+                    n=0
+                    print ""
+                    
+    def print_entities(self):
+        for i in self.__entities:
+            for j in i:
+                print j 
+    
 
 if __name__ == '__main__':
     
     manager = EManager()
-    manager.add_entity("movistar")
+    #manager.add_entity("movistar")
+    atributo = ["el presidente arruino todo" , "president arruin todo" , 0]
+    #manager.add_attribute("movistar", atributo)
     
-    entidades = manager.load_entities()
-    print entidades
+    #manager.add_entity("empresa")
+    #manager.add_attribute("empresa" , atributo)
+    
+    manager.find_polarity(["movistar" , "empresa, claro"])
+    manager.print_entities()
+    
+    
     
     
