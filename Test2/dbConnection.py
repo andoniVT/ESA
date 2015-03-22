71da07517f47f5d3765968c47b28520affa6b3f1
andoniVT <andoni.valverde@ucsp.edu.pe>
2015-03-22 12:35:52 -0500

    esa2

--- Test2/dbConnection.py
+++ Test2/dbConnection.py
@@ -13,7 +13,8 @@ class Connection(object):
     def __init__(self):
         self.__server = "localhost"
         self.__user = "root"
-        self.__pass = "jorgeandoni"
+        #self.__pass = "jorgeandoni"
+        self.__pass = "bayern"
         #self.__db = "test"
         self.__db = "esa"
         self.__conn = MySQLdb.connect(self.__server, self.__user, self.__pass, self.__db)
@@ -23,13 +24,44 @@ class Connection(object):
         c.execute("SELECT * FROM persona")
         rows = c.fetchall()
         for i in rows:
-            print i 
+            print i
+    
+    def add_entity(self, nombre_entidad):
+        c = self.__conn.cursor()
+        consulta =  'SELECT * FROM entidad WHERE nombre="%s"' % (nombre_entidad,) + ";"        
+        c.execute(consulta)
+        rows = c.fetchall()
+        if len(rows) == 0:
+            consulta2 = 'INSERT INTO entidad VALUES (NULL, "%s")' % (nombre_entidad,) + ";"
+            c.execute(consulta2)
+            print consulta2
+        else:
+            print "Entidad ya existe!"
+    
+    def add_attribute(self, nombre_entidad, atributo):
+        c = self.__conn.cursor()
+        consulta = 'SELECT id_entidad FROM entidad WHERE nombre="%s"' %(nombre_entidad,) + ";"
+        c.execute(consulta)
+        rows = c.fetchall()
+        if len(rows)!=0:            
+            id_entidad = rows[0][0]
+            consulta2 = 'INSERT INTO comentario2 VALUES (NULL, "%s"' % (atributo[0],) + ", " + str(atributo[1]) + ", " + str(id_entidad) + ");"
+            c.execute(consulta2)
+            print consulta2
+            
+        else:
+            print "Entidad no existe"
+                  
 
 
 if __name__ == '__main__':
     
     con = Connection()
-    #con.test()                    
+    #con.add_entity("claro")                   
+    
+    atributo = ["claro es una pesima empresa" , -1]
+    
+    con.add_attribute("movistar", atributo)
     
     
         
---
+++ Test2/textSegmentation.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+'''
+Created on 18/3/2015
+
+@author: ucsp
+'''
+from nltk import tokenize
+import itertools
+import re
+from __builtin__ import True
+
+class Segmentation(object):
+    
+    def __init__(self , sentence):
+        self.abbreviations = {'dr.': 'doctor', 'sr.': 'senior',  'sra.': 'seniora', 'srta.': 'seniorita', 'vs.': 'versus'}
+        self.terminators = ['.', '!', '?' , ',' , ';' ,'pero' , 'sin embargo' , 'no obstante', 'por el contrario' ,
+                               'en cambio' , 'con todo' , 'de todas maneras' , 'aunque', 'tampoco' ,
+                                'porque' , 'por esta razon', 'por consiguiente' ,'asi pues' , 'de ahi que', 
+                                'asi que', 'de modo que', 'es decir' , 'yy' , 'o sea', 'esto es', 'mejor dicho', 
+                                'por ejemplo' , 'oo' ]
+        self.wrappers = ['"', "'", ')', ']', '}']
+        self.sentence = sentence
+    
+    def find_all(self, a_str, sub):
+        start = 0
+        while True:
+            start = a_str.find(sub, start)
+            if start == -1:
+                return
+            yield start
+            start += len(sub)
+    
+    def find_sentence_end(self ,paragraph):
+        [possible_endings, contraction_locations] = [[], []]
+        contractions = self.abbreviations.keys()
+        sentence_terminators = self.terminators + [terminator + wrapper for wrapper in self.wrappers for terminator in self.terminators]
+        for sentence_terminator in sentence_terminators:
+            t_indices = list(self.find_all(paragraph, sentence_terminator))
+            possible_endings.extend(([] if not len(t_indices) else [[i, len(sentence_terminator)] for i in t_indices]))
+        for contraction in contractions:
+            c_indices = list(self.find_all(paragraph, contraction))
+            contraction_locations.extend(([] if not len(c_indices) else [i + len(contraction) for i in c_indices]))
+        possible_endings = [pe for pe in possible_endings if pe[0] + pe[1] not in contraction_locations]
+        if len(paragraph) in [pe[0] + pe[1] for pe in possible_endings]:
+            max_end_start = max([pe[0] for pe in possible_endings])
+            possible_endings = [pe for pe in possible_endings if pe[0] != max_end_start]
+        possible_endings = [pe[0] + pe[1] for pe in possible_endings if sum(pe) > len(paragraph) or (sum(pe) < len(paragraph) and paragraph[sum(pe)] == ' ')]
+        end = (-1 if not len(possible_endings) else max(possible_endings))
+        return end 
+    
+    def find_sentences(self):
+        especial_terminators = ["o" , "y" , "u"]
+        list_sentence = self.sentence.split()
+        sentence = ""
+        for i in list_sentence:
+            if i in especial_terminators:
+                i = i+i
+            sentence = sentence + i + " "
+        self.sentence = sentence  
+                
+        end = True
+        sentences = []
+        while end > -1:
+            end = self.find_sentence_end(self.sentence)
+            if end > -1:
+                sentences.append(self.sentence[end:].strip())
+                self.sentence = self.sentence[:end]
+        sentences.append(self.sentence)
+        sentences.reverse()
+        return sentences     
+        
+if __name__ == '__main__':
+    
+    seg = Segmentation("Felicitaciones al Real Madrid, en las buenas y en las malas Visca Barza!")
+    
+    segmentos = seg.find_sentences()
+    for i in segmentos:
+        print i 
+    
