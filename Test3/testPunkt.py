62d0ab1cada2957fa42e17e4d2d114de3b9a3612
andoniVT <andoni.valverde@ucsp.edu.pe>
2015-03-31 13:18:23 -0500

    testing punkt

--- Test3/testPunkt.py
+++ Test3/testPunkt.py
@@ -4,6 +4,24 @@ Created on 31/3/2015
 @author: ucsp
 '''
 
+import nltk.tokenize.punkt
+import pickle
+import codecs
+
+filename = "testTrain.txt"
+filename2 = "testTrain.pk"
+
+tokenizer = nltk.tokenize.punkt.PunktSentenceTokenizer()
+text = codecs.open(filename, "r", "utf8").read()
+tokenizer.train(text)
+out = open(filename2 , "wb")
+pickle.dump(tokenizer, out)
+out.close()
+
+
+
+
+
 if __name__ == '__main__':
     
-    pass 
+    print "hello!" 
